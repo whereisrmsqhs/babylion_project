3baa0aadadb167f0d5e106f33b222ca3ecafcee1
whereisrmsqhs <102295613+whereisrmsqhs@users.noreply.github.com>
2022-06-16 23:52:54 +0900

    first week project

--- project/projectapp/models.py
+++ project/projectapp/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     video = EmbedVideoField()
+    photo = models.ImageField(blank=True, null=True, upload_to='commu_photo')
 
     def __str__(self):
         return self.title
